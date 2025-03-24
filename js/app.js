9a696cf679ea2df12361a9ed126cb1226051cfac
Pat-devs <patryk.kodehode@gmail.com>
2025-03-24 10:47:12 +0100

    add error message handling I

--- js/app.js
+++ js/app.js
@@ -2,7 +2,7 @@
 const inputTodoEl = document.getElementById("input-todo-text")
 const addTodoButtonEl = document.getElementById("add-todo-button")
 const todoItemsContainer = document.getElementById("todo-items")
-
+const errorEl = document.getElementById("error")
 
 
 // Add event to the "form"
@@ -12,9 +12,17 @@ addTodoButtonEl.addEventListener("click", addTodoHandler)
 function addTodoHandler() {
     // 1. read input value
     let todoText = inputTodoEl.value
-    // 2. clear the input field
+    // 2. check if input is valid 
+    if (todoText == "") {
+        let errorMessage = "Todo item cannot be empty!"
+        errorEl.textContent = errorMessage
+        
+        return // if there are errors, return will prevent execution of code below this line
+    }
+
+    // 3. clear the input field
     inputTodoEl.value = ""
-    // 3. do something with the input text...
+    // 4. do something with the input text...
     console.log(todoText)
     
 
@@ -29,12 +37,8 @@ function addTodoHandler() {
 
 // creates a todo element and sets its text
 function createTodoElements(todoText) {
-
-    if (todoText == "") {
-        alert("Todo item cannot be empty!")
-    }
-    
-    const myElement = document.createElement("h1")
+  
+    const myElement = document.createElement("p")
     myElement.innerText = todoText
 
     todoItemsContainer.append(myElement)
